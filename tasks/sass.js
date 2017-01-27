4450c10b815635a828917b1442c96a12da59d00e
Sonia Marquette <smarquette@pixelcircus.ca>
2017-01-27 16:55:44 -0500

    Bugfix sass compilation not working

--- tasks/sass.js
+++ tasks/sass.js
@@ -2,9 +2,9 @@ module.exports = function (gulp, plugins, opts) {
     return function () {
         var args = plugins.yargs.argv,
             assets_folder = opts.compileDir+opts.assetsDir;
-        return plugins.rubySass(opts.sass.glob)
+        return gulp.src(opts.sass.glob).pipe(plugins.sass())
                 .pipe(plugins.autoprefixer({browsers: opts.sass.browsers}))
-                .on('error', plugins.rubySass.logError)
+                .on('error', plugins.sass.logError)
                 .pipe(gulp.dest(assets_folder + opts.sass.outputDir))
                 .pipe(plugins.browserSync.reload({stream: true}));
     };
